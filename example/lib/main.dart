1c9a7e92544c98728eb61ae9e941a77e29813f2c
dhwanish-3 <108540722+dhwanish-3@users.no-reply.github.com>
2026-01-12 00:09:31 +0530

    Upgrade start

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Example for Calendar Slider',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
